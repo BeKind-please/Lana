952e4997dcf9fd85aa59ce59e8a89d6ca4606be5
Svetlana Nikitina <anaconda100300@gmail.com>
2020-11-28 11:25:20 +0200

    study

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "# Lana" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/BeKind-please/Lana.git
+git push -u origin main
